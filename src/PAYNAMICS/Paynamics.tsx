e3d45a3b3ec2375386e51ae353c11f5ab6967e7c
frankgonzales22 <franksg@stpeter.com.ph>
2024-09-18 17:51:01 +0800

    dd

--- src/PAYNAMICS/Paynamics.tsx
+++ src/PAYNAMICS/Paynamics.tsx
@@ -12,7 +12,7 @@ const Paynamics = () => {
       
         return signature;
       };
-      console.log('eysssy')
+      console.log('eyssssssy')
       const makePaymentRequest = (params : any, merchantKey : string) => {
         // Generate signature
         const signature = generateSignature(params, merchantKey);
